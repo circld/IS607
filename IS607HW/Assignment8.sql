b8d2353bd3349dc1ccb276914a54af77f9564ad6
circld <circld1@gmail.com>
2014-10-21 20:44:53 -0600

    HW8 & ERD diagram.

---
+++ IS607HW/Assignment8.sql
@@ -0,0 +1,18 @@
+﻿-- IS607 HW 8
+-- Paul Garaud
+
+-- 1. Perform an environmental scan
+-- url: www.ritholtz.com
+
+-- 2. Design the logical database
+-- see image
+
+-- 3. Implement the physical db with sample data
+CREATE TABLE Post (
+	post_id SERIAL PRIMARY KEY ,
+	title VARCHAR(100) ,
+	content text ,
+	p_datetime DATETIME ,
+	views BIGINT
+);
+
