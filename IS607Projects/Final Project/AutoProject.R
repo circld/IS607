13fbbf28b1ad795a9a426d8e6366f2004e40b6c0
circld <circld1@gmail.com>
2014-12-02 07:20:25 -0700

    Added final project directory.

---
+++ IS607Projects/Final Project/AutoProject.R
@@ -0,0 +1,11 @@
+# What is the primary driver of auto sales, economic growth or employment?
+# Auto data:
+# http://www.motorintelligence.com/m_frameset.html
+# Economic growth (GDP?):
+# http://www.motorintelligence.com/m_frameset.html
+# Employment rate:
+# BLS
+
+# Linear regression
+# PCA?
+# Random Forests?
