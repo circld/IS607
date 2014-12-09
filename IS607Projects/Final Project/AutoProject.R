578e0227812abb662d8154b26ef76ad2565634b1
circld <circld1@gmail.com>
2014-12-09 15:37:32 -0700

    Data acquisition complete.

--- IS607Projects/Final Project/AutoProject.R
+++ IS607Projects/Final Project/AutoProject.R
@@ -11,12 +11,12 @@
 
 require(httr)
 require(XML)
+require(xlsx)
 require(stringr)
 
-# Data acquisition
+#### Data acquisition ####
 
 # Getting Auto sales data (millions of units)
-ua <- "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:33.0) Gecko/20100101 Firefox/33.0"
 fred.url <- 'http://research.stlouisfed.org/fred2/series/TOTALSA/downloaddata'
 fred.query <- list('form[native_frequency]' = 'Monthly', 
                    'form[units]' = 'lin',
@@ -24,11 +24,20 @@ fred.query <- list('form[native_frequency]' = 'Monthly',
                     'form[obs_end_date]' = "2014-11-01", 'form[file_format]' = 'txt',
                     'form[aggregation]' = 'Average',
                     sep = '&')
-fred.response <- POST(fred.url, body = fred.query, user_agent(ua))
+fred.response <- POST(fred.url, body = fred.query)
 fred.con <- textConnection(content(fred.response, 'text'))
 fred.data <- read.table(fred.con, header = TRUE, sep = "", skip = 10)
 
-# Getting GDP data
+rm(list = c('fred.query', 'fred.response', 'fred.con'))
+
+# Getting GDP data (download file + read in using xlsx package)
+nber.url <- 'http://www.nber.org/cycles/BCDCFiguresData100920_ver5.xlsx'
+nber.dir <- choose.dir(caption = 'Select directory in which to save NBER data.')
+nber.loc <- str_c(nber.dir, '\\NBER_data.xlsx')
+download.file(nber.url, nber.loc, mode = 'wb')
+nber.data <- read.xlsx(nber.loc, sheetIndex = 2, startRow = 2)
+
+rm(list = c('nber.dir', 'nber.loc'))
 
 
 # Getting BLS data directly from the BLS website
@@ -58,3 +67,6 @@ bls.data <- read.csv(bls.con, skip = 11)
 bls.data <- bls.data[,c(-length(names(bls.data)))]  # get rid of 'X' col
 
 rm(list = c('bls.query', 'bls.response', 'bls.html', 'bls.raw', 'bls.con'))
+
+#### Data Munging ####
+nber.data <- nber.data[, c(-12, -24, -25, -26)]
