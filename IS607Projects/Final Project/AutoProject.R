2c7743bcc920b4dabc4e94db06d92976ac4113c3
circld <circld1@gmail.com>
2014-12-08 19:06:44 -0700

    BLS data pull section complete.

--- IS607Projects/Final Project/AutoProject.R
+++ IS607Projects/Final Project/AutoProject.R
@@ -1,11 +1,50 @@
 # What is the primary driver of auto sales, economic growth or employment?
 # Auto data:
-# http://www.motorintelligence.com/m_frameset.html
-# Economic growth (GDP?):
-# http://www.motorintelligence.com/m_frameset.html
-# Employment rate:
-# BLS
+# http://research.stlouisfed.org/fred2/series/TOTALSA/downloaddata
+# GDP: 
+# http://www.nber.org/cycles/BCDCFiguresData100920_ver5.xlsx
+# Unemployment rate:
+# http://data.bls.gov/timeseries/LNS14000000
 
-# Linear regression
-# PCA?
-# Random Forests?
+# Linear regression (benchmark)
+# Random Forests (final model)
+
+require(httr)
+require(XML)
+require(stringr)
+
+# Data acquisition
+
+# Getting Auto sales data
+
+
+# Getting GDP data
+
+
+# Getting BLS data directly from the BLS website
+bls.url <- 'http://data.bls.gov/timeseries/LNS14000000/pdq/SurveyOutputServlet'
+bls.query <- list(request_action = 'get_data',
+                  reformat = 'true',
+                  from_results_page = 'false',
+                  years_option = 'specific_years',
+                  delimiter = 'comma',
+                  output_type = 'default',
+                  output_view = 'data',
+                  to_year = 2014,
+                  from_year = 1948,
+                  output_format = 'text',
+                  original_output_type = 'default',
+                  annualAveragesRequested = 'false',
+                  include_graphs = 'false'
+                  )
+bls.response <- GET(bls.url, query = bls.query, encode = 'form')
+bls.html <- htmlTreeParse(readBin(bls.response$content,
+                                  'text'), 
+                          useInternalNodes = TRUE)
+bls.raw <- xpathApply(bls.html, "//div/pre", xmlValue)
+bls.con <- textConnection(str_replace_all(as.character(bls.raw),
+                          'Â', ''))
+bls.data <- read.csv(bls.con, skip = 11)
+bls.data <- bls.data[,c(-length(names(bls.data)))]  # get rid of 'X' col
+
+rm(list = c('bls.query', 'bls.response', 'bls.html', 'bls.raw', 'bls.con'))
